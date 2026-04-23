a7a3603ad507bd5cbd088227a0aa1e217678fd5a
PhonicSpider <phonicspider93@gmail.com>
2026-04-22 23:35:30 -0400

    - Added Ark survival selection

--- public/configs/index.js
+++ public/configs/index.js
@@ -2,11 +2,13 @@
 import { minecraft } from './minecraft.js';
 import { spaceEngineers } from './space-engineers.js';
 import { terraria } from './terraria.js';
+import { ark } from './ark.js';
 
 // 2. Export them as a single "Registry" object
 export const ServerTypeRegistry = {
     'minecraft': minecraft,
     'space-engineers': spaceEngineers,
-    'terraria': terraria
+    'terraria': terraria,
+    'ark': ark,
     // When you add a new game, just add a new line here!
 };
